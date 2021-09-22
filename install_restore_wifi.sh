38c08044ac71bc108d4fcd65cedf8be038ba7df4
Transistor427 <47721986+Transistor427@users.noreply.github.com>
2021-09-22 17:06:22 +0300

    Update install_restore_wifi.sh

--- install_restore_wifi.sh
+++ install_restore_wifi.sh
@@ -2,14 +2,19 @@
 
 #Выдача прав
 sudo chmod 777 /home/pi/restore_wifi/*
+
 #Создание бекапа
 mkdir /home/pi/restore_wifi/backup_files/
-sudo cp -R /home/pi/KlipperScreen/panels/system.py /home/pi/restore_wifi/backup_files/system.py.back
-sudo cp -R /home/pi/moonraker/moonraker/components/machine.py /home/pi/restore_wifi/backup_files/machine.py.back
+sudo cp -r /home/pi/KlipperScreen/panels/system.py /home/pi/restore_wifi/backup_files/system.py.back
+sudo cp -r /home/pi/moonraker/moonraker/components/machine.py /home/pi/restore_wifi/backup_files/machine.py.back
+
 #Удаление отбекапленных файлов
-sudo rm -R /home/pi/KlipperScreen/panels/system.py
-sudo rm -R /home/pi/moonraker/moonraker/components/machine.py
+sudo rm -r /home/pi/KlipperScreen/panels/system.py
+sudo rm -r /home/pi/moonraker/moonraker/components/machine.py
+
 #Копирование
-sudo cp -R /home/pi/restore_wifi/system.py /home/pi/KlipperScreen/panels/system.py
-sudo cp -R /home/pi/restore_wifi/backup_files/machine.py /home/pi/moonraker/moonraker/components/machine.py
+sudo cp -r /home/pi/restore_wifi/system.py /home/pi/KlipperScreen/panels/system.py
+sudo cp -r /home/pi/restore_wifi/backup_files/machine.py /home/pi/moonraker/moonraker/components/machine.py
+
+
 sudo reboot
