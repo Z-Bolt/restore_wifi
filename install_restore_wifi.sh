d2ed0dc28338fc54067be9cdc0e4105d6c792fda
Transistor427 <47721986+Transistor427@users.noreply.github.com>
2021-09-22 16:48:53 +0300

    v1.0

--- install_restore_wifi.sh
+++ install_restore_wifi.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#Выдача прав
+sudo chmod 777 ./
+#Создание бекапа
+mkdir ./backup_files
+sudo cp -R /home/pi/KlipperScreen/panels/system.py /home/pi/restore_wifi/backup_files/system.py.back
+sudo cp -R /home/pi/moonraker/moonraker/components/machine.py /home/pi/restore_wifi/backup_files/machine.py.back
+#Удаление отбекапленных файлов
+sudo rm -R /home/pi/KlipperScreen/panels/system.py
+sudo rm -R /home/pi/moonraker/moonraker/components/machine.py
+#Копирование
+sudo cp -R /home/pi/restore_wifi/system.py /home/pi/KlipperScreen/panels/system.py
+sudo cp -R /home/pi/restore_wifi/backup_files/machine.py /home/pi/moonraker/moonraker/components/machine.py
+sudo reboot
