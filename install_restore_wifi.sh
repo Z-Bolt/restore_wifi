46974fde503ae1efbbe4215ac7903230c0e5ca6e
Transistor427 <47721986+Transistor427@users.noreply.github.com>
2021-09-23 19:11:34 +0300

    Add files via upload

---
+++ install_restore_wifi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Выдача прав
+sudo chmod 777 /home/pi/restore_wifi/*
+
+#Создание бекапа
+mkdir /home/pi/restore_wifi/backup_files/
+sudo cp -r /home/pi/KlipperScreen/panels/system.py /home/pi/restore_wifi/backup_files/system.py.back
+sudo cp -r /home/pi/moonraker/moonraker/components/machine.py /home/pi/restore_wifi/backup_files/machine.py.back
+
+#Удаление отбекапленных файлов
+sudo rm -r /home/pi/KlipperScreen/panels/system.py
+sudo rm -r /home/pi/moonraker/moonraker/components/machine.py
+
+#Копирование
+sudo cp -r /home/pi/restore_wifi/system.py /home/pi/KlipperScreen/panels/system.py
+sudo cp -r /home/pi/restore_wifi/machine.py /home/pi/moonraker/moonraker/components/machine.py
+sudo cp -r /home/pi/restore_wifi/network_restore.svg /home/pi/KlipperScreen/styles/z-bolt/images/network_restore.svg
+
+#Перезагрузка
+sudo reboot
