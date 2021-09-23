167e6219e412fc3ab64f5f2fbc004895f0e4acdc
Transistor427 <47721986+Transistor427@users.noreply.github.com>
2021-09-23 18:29:24 +0300

    Update restore_wifi.sh

--- restore_wifi.sh
+++ restore_wifi.sh
@@ -5,7 +5,7 @@ sudo systemctl stop KlipperScreen.service
 sudo systemctl stop networking.service
 #Move file /etc/wpa_supplicant
 sudo rm -R /etc/wpa_supplicant/wpa_supplicant.conf
-sudo cp -R /home/pi/restore/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp -R /home/pi/restore_wifi/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 #Reboot
 sudo systemctl start klipper.service
 sudo systemctl start moonraker.service
